706aaf489066ed23016d9743d6d82e8737959eaf
zaynab-lb <zaynablboudaoudi@gmail.com>
2025-05-21 19:52:54 +0100

    Internationalisation et Localisation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,25 @@ export const routes: Routes =
     {path : 'cart', component : CartComponent, title: 'My cart'},
     {path : 'signin', component : SigninComponent, title: 'My signin page'},
     {path : '', redirectTo : '/catalog', pathMatch : 'full'},
-    { path: 'product-details/:id', component: ProductDetailsComponent, title: 'Product details' }
+    {path: 'product-details/:id', component: ProductDetailsComponent, title: 'Product details'}
+
+    /*{
+    path: 'product-details/:id',
+    component: ProductDetailsComponent,
+    title: 'Product details',
+    // Cette propriété permet à Angular de pré-rendre les pages pour chaque ID listé
+    data: {
+      prerender: true,
+      getPrerenderParams: () => {
+        return [
+          { id: 1 },
+          { id: 2 },
+          { id: 3 },
+          { id: 4 },
+          { id: 5 }
+        ];
+      }
+    }
+  }*/
 
 ];
--- src/product-details-component/product-details-component.component.spec.ts
+++ src/product-details-component/product-details-component.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductDetailsComponentComponent } from './product-details-component.component';
+import { ProductDetailsComponent } from './product-details-component.component';
 
 describe('ProductDetailsComponentComponent', () => {
-  let component: ProductDetailsComponentComponent;
-  let fixture: ComponentFixture<ProductDetailsComponentComponent>;
+  let component: ProductDetailsComponent;
+  let fixture: ComponentFixture<ProductDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductDetailsComponentComponent]
+      imports: [ProductDetailsComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ProductDetailsComponentComponent);
+    fixture = TestBed.createComponent(ProductDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
